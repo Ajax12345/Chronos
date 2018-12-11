822fb9cc780087a00f12b202d3329cde8974d01a
James Petullo <pentester18@gmail.com>
2018-12-10 21:12:14 -0500

    Update chronos_utilities.py

--- chronos_utilities.py
+++ chronos_utilities.py
@@ -1,5 +1,5 @@
-import tigerSqlite, sqlite3, typing
-import datetime
+import tigerSqlite, sqlite3, typing, re
+import datetime, calendar, functools
 
 def validate_parameters(_f:typing.Callable[[typing.Callable, dict], typing.Callable]) -> typing.Callable:
     def _wrapper(_cls, **_kwargs) -> typing.Any:
@@ -17,3 +17,23 @@ def parse_datetime(_f:typing.Callable) -> typing.Callable:
         return _f(cls, 12 if month < 1 else 1 if month > 12 else month, year - 1 if month < 1 else year + 1 if month > 12 else year)
 
     return _wrapper
+
+def _parse_year_day(_payload:dict) -> typing.Tuple[int, int]:
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    return int(_payload['year']) if isinstance(_payload['year'], str) else _payload['year'], months.index(_payload['month'])+1 if isinstance(_payload['month'], str) else _payload['month']
+
+def rangeify(_f:typing.Callable) -> typing.Callable:
+    @functools.wraps(_f)
+    def _wrapper(_cls, **kwargs:dict) -> typing.Callable:
+        if 'user' not in kwargs:
+            raise TypeError(f'"{_f.__name__}" requires a user id')
+        if kwargs.get('expedient', False):
+            _d = datetime.datetime.now()
+            _new_d = datetime.date(*[getattr(_d, i) for i in ['year', 'month', 'day']])
+            _week = [i for i in calendar.Calendar().monthdatescalendar(_d.year, _d.month) if any(c == _new_d for c in i)][0]
+            return _f(_cls, kwargs.get('user'), _week)
+        _day1, _day2 = map(int, re.findall('\d+', kwargs['dayrange']))
+        _week = [i for i in calendar.Calendar().monthdatescalendar(*_parse_year_day(kwargs)) if i[0].day == _day1 and i[-1].day == _day2][0]
+        return _f(_cls, kwargs.get('user'), _week)
+    return _wrapper
+
