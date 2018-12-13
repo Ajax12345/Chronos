1684ef8bfe93f6e75d50cab8b3fec600320b7000
James Petullo <pentester18@gmail.com>
2018-12-12 22:41:34 -0500

    Update user_calendar.py

--- user_calendar.py
+++ user_calendar.py
@@ -21,8 +21,16 @@ class Event:
     def __iter__(self):
         yield from [(a, b) for a, b in self.__dict__.items()]
     def __eq__(self, _event) -> bool:
-        _event1, _event2 = Calendar.event_datetime(dict(self)), Calendar.event_datetime(_event) 
-        return all(a == b for a, b in zip(_event1.full_range, _event2.full_range))
+        _event1, _event2 = Calendar.event_datetime(dict(self)), Calendar.event_datetime(dict(_event)) 
+        print('ranges')
+        print(_event1.full_range, _event2.full_range)
+        if all(a == b for a, b in zip(_event1.full_range, _event2.full_range)):
+            return True
+        a, b = _event1.full_range, _event2.full_range
+        if all(all(getattr(c, i) == getattr(d, i) for i in ['year', 'month', 'day', 'hour']) for c, d in zip(a, b)) and abs(a[-1].day - b[-1].day) < 5:
+            return True
+        return False
+        
 
 class _by_week_display:
     def __init__(self, _day_id:int, _day:datetime.date) -> None:
@@ -92,15 +100,25 @@ class _dayColumn:
             print('event here', event)
             hour1, minutes1, meridian1, hour2, minutes2, meridian2 = re.findall('\d+(?=:)|(?<=:)\d+|[AMP]+', event['timerange'])
             hour1, minutes1, hour2, minutes2 = int(hour1) if meridian1 == 'AM' else int(hour1) + 12, int(minutes1), int(hour2) if meridian2 == 'AM' else int(hour2)+12, int(minutes2)
-            for _ in range(start, hour1+start-1):
+            print('start before event', start)
+            print('range values: ', hour1, start)
+            for _ in range(hour1-start-1):
                 yield _day_obj(self.header.rounded_day, next(_counter))
                 start += 1
+            print('start after event', start)
+            
             _e = Event(event)
             _e.parent_height = 45*((hour2 if not minutes2 else hour2+1)-hour1)
+            print('event minutes', minutes1)
             _e.from_top = int((minutes1/float(60))*45)
-            _e.height = (45-_e.from_top)+(hour2-(hour1+1))+(0 if not minutes2 else int((minutes2/float(60))*45))
+            print('from _top', _e.from_top)
+            #_e.height = (45-_e.from_top)+((hour2-(hour1+1))*45)+(0 if not minutes2 else int((minutes2/float(60))*45))
+            _e.height = (45-_e.from_top)+((hour2-(hour1+1))*45)+(0 if not minutes2 else int((minutes2/float(60))*45))
+            print('height result: ', _e.height)
             yield _day_obj(self.header.rounded_day, next(_counter), event = _e)
             start += _e.parent_height//45
+            print('final start', start)
+            print('-'*20)
         for _ in range(start, 24):
             yield _day_obj(self.header.rounded_day, next(_counter))
     
@@ -118,6 +136,7 @@ class Calendar:
     """
     def __init__(self, _data:list, **kwargs:dict) -> None:
         self.full_data = _data
+        self._week_row = [a for a, _ in self.full_data]
         self.__dict__.update({i:kwargs.get(i) for i in ['month', 'year', 'by_week']})
         print('kwargs in here', kwargs)
         print('month test here', self.month)
@@ -127,6 +146,49 @@ class Calendar:
             raise AttributeError('calendar instance by month, not week')
         return 'Monday {} - Sunday {}'.format(*self.__class__.singletion_dates(self.full_data))
     
+    @property
+    def previous_month(self):
+        if not self.by_week:
+            raise AttributeError('calendar instance by month, not week')
+        day1, day2 = self._week_row[0] - datetime.timedelta(days=7), self._week_row[-1] - datetime.timedelta(days=7)
+        return Calendar.months[day1.month-1]
+
+    @property
+    def next_month(self):
+        if not self.by_week:
+            raise AttributeError('calendar instance by month, not week')
+        day1, day2 = self._week_row[0] + datetime.timedelta(days=7), self._week_row[-1] + datetime.timedelta(days=7)
+        return Calendar.months[day1.month-1]
+
+    @property
+    def next_year(self):
+        if not self.by_week:
+            raise AttributeError('calendar instance by month, not week')
+        day1, day2 = self._week_row[0] + datetime.timedelta(days=7), self._week_row[-1] + datetime.timedelta(days=7)
+        return day1.year
+
+    @property
+    def previous_year(self):
+        if not self.by_week:
+            raise AttributeError('calendar instance by month, not week')
+        day1, day2 = self._week_row[0] - datetime.timedelta(days=7), self._week_row[-1] - datetime.timedelta(days=7)
+        return day1.year
+
+    @property
+    def previous_dayrange(self):
+        if not self.by_week:
+            raise AttributeError('calendar instance by month, not week')
+        day1, day2 = self._week_row[0] - datetime.timedelta(days=7), self._week_row[-1] - datetime.timedelta(days=7)
+        #_day1, _day2 = day1 if day1 > 0 else (30 if self.previous_month in {'September', 'April', 'June', 'November'} else 31) - abs(day1), day2 if day2 > 0 else (30 if self.previous_month in {'September', 'April', 'June', 'November'} else 31) - abs(day2)
+        return f'{day1.day}_{day2.day}'
+    
+    @property
+    def next_dayrange(self):
+        if not self.by_week:
+            raise AttributeError('calendar instance by month, not week')
+        day1, day2 = self._week_row[0] + datetime.timedelta(days=7), self._week_row[-1] + datetime.timedelta(days=7)
+        #_day1, _day2 = day1 if day1 > 0 else (30 if self.previous_month in {'September', 'April', 'June', 'November'} else 31) - abs(day1), day2 if day2 > 0 else (30 if self.previous_month in {'September', 'April', 'June', 'November'} else 31) - abs(day2)
+        return f'{day1.day}_{day2.day}'
 
     @property
     def by_week_row(self):
@@ -150,6 +212,7 @@ class Calendar:
     
     @classmethod
     def event_datetime(cls, _event:dict) -> datetime.datetime:
+        print('got event here', _event)
         day, _hour = map(int, re.findall('\d+', _event['parent_id']))
         _start, _end = map(int, re.findall('\d+', _event['week_range']))
         full_calendar = calendar.Calendar().monthdatescalendar(int(_event['year']), cls.months.index(_event['month'])+1)
@@ -159,17 +222,22 @@ class Calendar:
         return _event_datetime(full_day, _event['timerange'], full_timerange)
     
     @classmethod
+    def _event_sort_key(cls, _event:dict) -> typing.Tuple[int, int]:
+        hour1, minutes1, meridian1, hour2, minutes2, meridian2 = re.findall('\d+(?=:)|(?<=:)\d+|[APM]+', _event['timerange'])
+        return (int(hour1)+(0 if meridian1 == 'AM' else 12)+int(minutes1), int(hour2)+(0 if meridian2 == 'AM' else 12)+int(minutes2))
+
+    @classmethod
     def create_calendar_event(cls, _user:int, _payload:dict) -> None:
         _timestamps = cls.event_datetime(_payload)
         new_payload = {'timestamp':str(_timestamps.day), 'created_on':str(datetime.datetime.now()), **_payload}
         print('resulting payload', new_payload)
         current_events = [b for a, b in tigerSqlite.Sqlite('user_calendars.db').get_id_events('calendars') if a == _user][-1]
-        tigerSqlite.Sqlite('user_calendars.db').update('calendars', [['events', current_events+[new_payload]]], [['id', _user]])
+        tigerSqlite.Sqlite('user_calendars.db').update('calendars', [['events', sorted(current_events+[new_payload], key=cls._event_sort_key)]], [['id', _user]])
         
     @classmethod
     def quick_look(cls, _user:int, _payload:dict) -> typing.Callable:
         _user_events = list(map(Event, [b for a, b in tigerSqlite.Sqlite('user_calendars.db').get_id_events('calendars') if a == _user][-1]))
-        return [Event(i) for i in _user_events if i == Event(_payload)][0]
+        return [i for i in _user_events if i == Event(_payload)][0]
 
     @classmethod
     @chronos_utilities.rangeify
@@ -183,6 +251,21 @@ class Calendar:
         _all_events =[b for a, b in tigerSqlite.Sqlite('user_calendars.db').get_id_events('calendars') if int(a) == int(_user)][0]
         _ranges = [[i, [a for a in _all_events if datetime.date(*map(int, re.findall('\d+', a['timestamp']))) == i]] for i in _week]
         return cls(_ranges, by_week = True, year=year, month=month)
+
+    @classmethod
+    def remove_event(cls, _user:int, _payload:dict) -> typing.Callable:
+        _all_events =[b for a, b in tigerSqlite.Sqlite('user_calendars.db').get_id_events('calendars') if int(a) == int(_user)][0]
+        new_events = [i for i in _all_events if Event(i) != Event(_payload)]
+        tigerSqlite.Sqlite('user_calendars.db').update('calendars', [['events', new_events]], [['id', int(_user)]])
+        return cls.by_week(user=_user, expedient=True)
+    
+    @classmethod
+    def navigate_week(cls, _user:int, payload:dict) -> typing.Callable:
+        _calendar = calendar.Calendar().monthdatescalendar(int(payload['year']), cls.months.index(payload['month'])+1)
+        _day1, _day2 = map(int, re.findall('\d+', payload['dayrange']))
+        _new_week = [i for i in _calendar if i[0].day == _day1 and i[-1].day == _day2][0]
+        return cls._by_week(_user, _new_week, int(payload['year']), payload['month'])
+
 
     @classmethod
     def navigate_by_week(cls, _user:int, _payload:dict) -> typing.Callable:
